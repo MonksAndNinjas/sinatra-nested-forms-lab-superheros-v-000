91876f6706aaf1e3afa0dc07b5f4acbe3e860f23
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-05 19:43:41 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,6 @@
 require 'active_record'
 
-DB = ActiveRecord::Base.establish_connection(
+ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/teams.sqlite"
 )
-
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS teams (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    motto TEXT
-  )
-SQL
-
-ActiveRecord::Base.connection.execute(sql)
