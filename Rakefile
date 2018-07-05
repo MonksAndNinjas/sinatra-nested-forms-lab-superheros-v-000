119c70dc9a74a549361921aeba3f87dbfca90407
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-05 19:33:51 +0000

    automatically backed up by learn

--- Rakefile
+++
@@ -1,8 +0,0 @@
-ENV["SINATRA_ENV"] = "development"
-
-require_relative './environemnt'
-require 'sinatra/activerecord/rake'
-
-task :console do
-  Pry.start
-end
---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+require 'active_record'
+
+DB = ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3"
+  :database => "../db/teams"
+)
+
+  DB = ActiveRecord::Base.connection
+
+  if ENV["ACTIVE_RECORD_ENV"] == "development"
+    ActiveRecord::Migration.verbose = false
