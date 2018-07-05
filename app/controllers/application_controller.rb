ed782e02693924099cc97a268478e06999c8a8e9
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-05 18:26:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,9 @@ class App < Sinatra::Base
     post '/teams' do
       @team = Team.new(params[:team])
 
-      @superheroes = params[:team][:superheroes]
+      @superheroes = params[:team][:superheroes].each do |details|
+        
+      end
 
       erb :team
     end
