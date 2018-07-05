97ee1ee5b46ca568a7a3dbf0ebab4c642f693438
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-05 20:03:25 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,9 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+connection = ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/teams.sqlite"
+)
+
 require_all 'app'
